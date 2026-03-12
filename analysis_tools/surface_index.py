fdd694059da7ed7608b78f7e20d7ddce1f1bfcaa
wagwag112 <huyuyhoangoang112@gmail.com>
2026-03-12 15:57:41 +0800

    Create surface_index.py

---
+++ analysis_tools/surface_index.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import sys
+import ase.io as io
+
+INPUT_STRUCTURE = "POSCAR"
+
+if len(sys.argv) != 3:
+    print("Usage: python find_surface_atoms.py axis thickness_A")
+    sys.exit(1)
+
+axis = sys.argv[1].lower()
+X_LIMIT = float(sys.argv[2])
+
+axis_map = {"x":0, "y":1, "z":2}
+
+if axis not in axis_map:
+    print("Axis must be x, y, or z")
+    sys.exit(1)
+
+ax = axis_map[axis]
+
+# --- LOAD STRUCTURE ---
+atoms = io.read(INPUT_STRUCTURE)
+
+pos = atoms.get_positions()
+cell = atoms.get_cell().lengths()
+
+coord = pos[:,ax]
+L = cell[ax]
+
+# --- FIND ATOMS ---
+selected_index = [
+    i for i in range(len(atoms))
+    if (coord[i] <= X_LIMIT) or (coord[i] >= L - X_LIMIT)
+]
+
+# --- OUTPUT ---
+print(f"Axis: {axis}")
+print(f"Thickness: {X_LIMIT} A")
+print(f"Selected atoms: {len(selected_index)}")
+print(selected_index)
